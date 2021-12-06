a403f9e90412b2c40d2025282903682efcd1bdc6
Andreas Leibrock <aleibrock@vwd.com>
2021-12-06 17:14:57 +0100

    day 6 part 2 refactor

--- src/main/java/net/leibi/adventofcode2021/day6/Day62021.java
+++ src/main/java/net/leibi/adventofcode2021/day6/Day62021.java
@@ -8,36 +8,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Day62021 {
 
-
-
   static List<Integer> getFishesFromInput(String input) {
-    return Arrays.stream(getIntArrayFromString(input.trim(), ","))
-        .boxed()
-        .collect(Collectors.toList());
+    return Arrays.stream(getIntArrayFromString(input.trim(), ",")).boxed().toList();
   }
 
-   static BigDecimal getNumberOfFishes(List<Integer> inputIntegers,int countTo) {
+   static BigDecimal getNumberOfFishes(List<Integer> inputIntegers,int days) {
     var map = inputIntegers
         .stream()
         .collect(Collectors.groupingBy(Integer::intValue))
         .entrySet()
         .stream()
         .collect(Collectors.toMap(Map.Entry::getKey, e -> new BigDecimal(e.getValue().size())));
-    for (int i = 0; i < countTo; i++) {
+    for (int i = 0; i < days; i++) {
       Map<Integer, BigDecimal> newMap = new HashMap<>();
       BigDecimal newFishes = BigDecimal.ZERO;
-      for (Integer key : map.keySet()) {
-        var value = map.get(key);
-        if (key == 0) {
-          newFishes = value;
-          newMap.merge(6, value, BigDecimal::add);
+      for (Map.Entry<Integer,BigDecimal> entry : map.entrySet()) {
+        if (entry.getKey() == 0) {
+          newFishes = entry.getValue();
+          newMap.merge(6, entry.getValue(), BigDecimal::add);
         } else {
-          newMap.merge(key - 1, value, BigDecimal::add);
+          newMap.merge(entry.getKey() - 1, entry.getValue(), BigDecimal::add);
         }
       }
       newMap.put(8, newFishes);
