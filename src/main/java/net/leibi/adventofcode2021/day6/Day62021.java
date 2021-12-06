90291f46985328c8f038c2b1dd30006113edb2b2
Andreas Leibrock <aleibrock@vwd.com>
2021-12-06 17:03:53 +0100

    day 6 part 2 complete

--- src/main/java/net/leibi/adventofcode2021/day6/Day62021.java
+++ src/main/java/net/leibi/adventofcode2021/day6/Day62021.java
@@ -2,24 +2,18 @@ package net.leibi.adventofcode2021.day6;
 
 import static net.leibi.helpers.InputHelper.getIntArrayFromString;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Day62021 {
 
-  List<Integer> fishes;
 
-  public Day62021(final String input) {
-    fishes = getFishesFromInput(input);
-  }
-
-  public void simulateDays(int numberOfDays) {
-
-    for (int i = 0; i < numberOfDays; i++) {
-      simulateDay();
-    }
-  }
 
   static List<Integer> getFishesFromInput(String input) {
     return Arrays.stream(getIntArrayFromString(input.trim(), ","))
@@ -27,19 +21,30 @@ public class Day62021 {
         .collect(Collectors.toList());
   }
 
-  private void simulateDay() {
-    int numberOfFishesToAdd = 0;
-    for (int i = 0; i < fishes.size(); i++) {
-      int currentFish = fishes.get(i);
-      if (currentFish == 0) {
-        fishes.set(i, 6);
-        numberOfFishesToAdd++;
-      } else {
-        fishes.set(i, --currentFish);
+   static BigDecimal getNumberOfFishes(List<Integer> inputIntegers,int countTo) {
+    var map = inputIntegers
+        .stream()
+        .collect(Collectors.groupingBy(Integer::intValue))
+        .entrySet()
+        .stream()
+        .collect(Collectors.toMap(Map.Entry::getKey, e -> new BigDecimal(e.getValue().size())));
+    for (int i = 0; i < countTo; i++) {
+      Map<Integer, BigDecimal> newMap = new HashMap<>();
+      BigDecimal newFishes = BigDecimal.ZERO;
+      for (Integer key : map.keySet()) {
+        var value = map.get(key);
+        if (key == 0) {
+          newFishes = value;
+          newMap.merge(6, value, BigDecimal::add);
+        } else {
+          newMap.merge(key - 1, value, BigDecimal::add);
+        }
       }
+      newMap.put(8, newFishes);
+      map = newMap;
     }
-    for (int i = 0; i < numberOfFishesToAdd; i++) {
-      fishes.add(8);
-    }
+
+    return map.values().stream().reduce(BigDecimal.ZERO, (BigDecimal::add));
   }
+
 }
--- src/test/java/net/leibi/adventofcode2021/day6/Day62021Test.java
+++ src/test/java/net/leibi/adventofcode2021/day6/Day62021Test.java
@@ -1,7 +1,9 @@
 package net.leibi.adventofcode2021.day6;
 
+import static net.leibi.adventofcode2021.day6.Day62021.getFishesFromInput;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -9,69 +11,29 @@ class Day62021Test {
 
   @Test
   void getFishesFromInputTest() {
-    List<Integer> fishes = Day62021.getFishesFromInput(Input.SHORT_INPUT);
+    List<Integer> fishes = getFishesFromInput(Input.SHORT_INPUT);
     assertThat(fishes).hasSize(5);
     assertThat(fishes.get(0)).isEqualTo(3);
     assertThat(fishes.get(4)).isEqualTo(2);
   }
 
   @Test
-  void testCtor() {
-    Day62021 day62021 = new Day62021(Input.SHORT_INPUT);
-    assertThat(day62021.fishes).isNotEmpty().hasSize(5);
-  }
-
-  @Test
-  void testfirstDay() {
-    Day62021 day62021 = new Day62021(Input.SHORT_INPUT);
-    day62021.simulateDays(1);
-    assertThat(day62021.fishes).isNotEmpty().hasSize(5);
-    assertThat(day62021.fishes.get(0)).isEqualTo(2);
-    assertThat(day62021.fishes.get(1)).isEqualTo(3);
-    assertThat(day62021.fishes.get(2)).isEqualTo(2);
-    assertThat(day62021.fishes.get(3)).isEqualTo(0);
-    assertThat(day62021.fishes.get(4)).isEqualTo(1);
-  }
-  @Test
-  void testTwoDays() {
-    Day62021 day62021 = new Day62021(Input.SHORT_INPUT);
-    day62021.simulateDays(2);
-    assertThat(day62021.fishes).isNotEmpty().hasSize(6);
-    assertThat(day62021.fishes.get(0)).isEqualTo(1);
-    assertThat(day62021.fishes.get(1)).isEqualTo(2);
-    assertThat(day62021.fishes.get(2)).isEqualTo(1);
-    assertThat(day62021.fishes.get(3)).isEqualTo(6);
-    assertThat(day62021.fishes.get(4)).isEqualTo(0);
-    assertThat(day62021.fishes.get(5)).isEqualTo(8);
-  }
-
-  @Test
   void test18Days() {
-    Day62021 day62021 = new Day62021(Input.SHORT_INPUT);
-    day62021.simulateDays(18);
-    assertThat(day62021.fishes).isNotEmpty().hasSize(26);
-    assertThat(day62021.fishes.get(0)).isEqualTo(6);
-    assertThat(day62021.fishes.get(25)).isEqualTo(8);
+    assertThat(Day62021.getNumberOfFishes(getFishesFromInput(Input.SHORT_INPUT),18)).isEqualTo(BigDecimal.valueOf(26));
   }
 
   @Test
   void test80Days() {
-    Day62021 day62021 = new Day62021(Input.SHORT_INPUT);
-    day62021.simulateDays(80);
-    assertThat(day62021.fishes).isNotEmpty().hasSize(5934);
+    assertThat(Day62021.getNumberOfFishes(getFishesFromInput(Input.SHORT_INPUT),80)).isEqualTo(BigDecimal.valueOf(5934));
  }
 
   @Test
   void test80DaysLong() {
-    Day62021 day62021 = new Day62021(Input.LONG_INPUT);
-    day62021.simulateDays(80);
-    assertThat(day62021.fishes).isNotEmpty().hasSize(390011);
+    assertThat(Day62021.getNumberOfFishes(getFishesFromInput(Input.LONG_INPUT),80)).isEqualTo(BigDecimal.valueOf(390011));
   }
 
   @Test
   void test256DaysLong() {
-    Day62021 day62021 = new Day62021(Input.LONG_INPUT);
-    day62021.simulateDays(256);
-    assertThat(day62021.fishes).isNotEmpty().hasSize(390011);
+    assertThat(Day62021.getNumberOfFishes(getFishesFromInput(Input.LONG_INPUT),256)).isEqualTo(BigDecimal.valueOf(1746710169834L));
   }
 }
