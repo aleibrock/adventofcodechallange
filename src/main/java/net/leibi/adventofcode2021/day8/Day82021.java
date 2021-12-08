8439e78e3136e17cf98f0cc17be4e54ccc25f278
Andreas Leibrock <aleibrock@vwd.com>
2021-12-08 07:47:03 +0100

    day 8 part 1 refactor

--- src/main/java/net/leibi/adventofcode2021/day8/Day82021.java
+++ src/main/java/net/leibi/adventofcode2021/day8/Day82021.java
@@ -5,7 +5,6 @@ import static net.leibi.helpers.InputHelper.getRowListFromInput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -14,25 +13,21 @@ public class Day82021 {
 
   private static final int[] valid_length = new int[] {2, 3, 4, 7};
 
-  static List<String> parseInput(final String input) {
+  static List<String> parseOutputValues(final String input) {
 
     List<String> result = new ArrayList<>();
 
     List<String> lines = getRowListFromInput(input);
     for (String line : lines) {
       result.addAll(
-          Arrays.stream(line.split("\\|")[1].split(" "))
-              .filter(s -> !s.isBlank())
-              .collect(Collectors.toList()));
+          Arrays.stream(line.split("\\|")[1].split(" ")).filter(s -> !s.isBlank()).toList());
     }
 
     return result;
   }
 
-  static int solve(final String input) {
-    List<String> output_values = parseInput(input);
-
-    return output_values.stream()
+  static int getNumberOfUniqueDigits(final String input) {
+    return parseOutputValues(input).stream()
         .filter(s -> Arrays.stream(valid_length).anyMatch(v -> v == s.trim().length()))
         .toList()
         .size();
--- src/test/java/net/leibi/adventofcode2021/day8/Day82021Test.java
+++ src/test/java/net/leibi/adventofcode2021/day8/Day82021Test.java
@@ -8,19 +8,19 @@ import org.junit.jupiter.api.Test;
 class Day82021Test {
 
   @Test
-  void parseInput() {
+  void parseOutputValues() {
 
-    List<String> values = Day82021.parseInput(Input.SHORT_INPUT);
+    List<String> values = Day82021.parseOutputValues(Input.SHORT_INPUT);
     assertThat(values).hasSize(40);
   }
 
   @Test
   void solveShort() {
-    assertThat(Day82021.solve(Input.SHORT_INPUT)).isEqualTo(26);
+    assertThat(Day82021.getNumberOfUniqueDigits(Input.SHORT_INPUT)).isEqualTo(26);
   }
 
   @Test
   void solveLong() {
-    assertThat(Day82021.solve(Input.LONG_INPUT)).isEqualTo(26);
+    assertThat(Day82021.getNumberOfUniqueDigits(Input.LONG_INPUT)).isEqualTo(409);
   }
 }
