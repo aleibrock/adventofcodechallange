24cc1d06dc3f5611cadb40f7d7d65e8b91575c48
Andreas Leibrock <aleibrock@vwd.com>
2021-12-09 08:18:30 +0100

    day 9 part 1 done

--- src/main/java/net/leibi/adventofcode2021/day9/Day92021.java
+++ src/main/java/net/leibi/adventofcode2021/day9/Day92021.java
@@ -1,9 +1,19 @@
 package net.leibi.adventofcode2021.day9;
 
+import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import net.leibi.helpers.InputHelper;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Day92021 {
+
+  static int solve(final String input) {
+
+    List<Integer> lowpoints = getLowPoints(parseInput(input));
+    return lowpoints.size() + lowpoints.stream().mapToInt(i -> i).sum();
+  }
 
   static int[][] parseInput(final String input) {
 
@@ -14,6 +24,43 @@ public class Day92021 {
     for (String s : rowList) {
       int[] row = InputHelper.getIntArrayFromString(s);
       result[currentRow++] = row;
+    }
+    return result;
+  }
+
+  static List<Integer> getLowPoints(int[][] matrix) {
+
+    List<Integer> result = new ArrayList<>();
+
+    int height = matrix.length;
+    int width = matrix[0].length;
+
+    for (int columnCursor = 0; columnCursor < height; columnCursor++) {
+      for (int rowCursor = 0; rowCursor < width; rowCursor++) {
+
+        int up = 10;
+        int down = 10;
+        int left = 10;
+        int right = 10;
+
+        int currentValue = matrix[columnCursor][rowCursor];
+        if (columnCursor > 0) {
+          up = matrix[columnCursor - 1][rowCursor];
+        }
+        if (columnCursor < height - 1) {
+          down = matrix[columnCursor + 1][rowCursor];
+        }
+        if (rowCursor < width - 1) {
+          right = matrix[columnCursor][rowCursor + 1];
+        }
+        if (rowCursor > 0) {
+          left = matrix[columnCursor][rowCursor - 1];
+        }
+
+        if (currentValue < Math.min(Math.min(Math.min(up, down), left), right)) {
+          result.add(currentValue);
+        }
+      }
     }
     return result;
   }
--- src/test/java/net/leibi/adventofcode2021/day9/Day92021Test.java
+++ src/test/java/net/leibi/adventofcode2021/day9/Day92021Test.java
@@ -1,11 +1,29 @@
 package net.leibi.adventofcode2021.day9;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class Day92021Test {
+
+  @Test
+  void solve() {
+    assertThat(Day92021.solve(Input.SHORT_INPUT)).isEqualTo(15);
+  }
+
+  @Test
+  void solveLong() {
+    assertThat(Day92021.solve(Input.LONG_INPUT)).isEqualTo(496);
+  }
+
+  @Test
+  void getLowPoints() {
+    int[][] matrix = Day92021.parseInput(Input.SHORT_INPUT);
+    List<Integer> lowPoints = Day92021.getLowPoints(matrix);
+
+    assertThat(lowPoints).hasSize(4).containsExactly(1, 0, 5, 5);
+  }
 
   @Test
   void parseInput() {
