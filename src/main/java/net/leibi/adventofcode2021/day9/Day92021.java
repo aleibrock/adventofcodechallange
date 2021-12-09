6b3b309fd5019c310b9097af6dda906c34126121
Andreas Leibrock <aleibrock@vwd.com>
2021-12-09 14:43:56 +0100

    day 9 part 2 done

--- src/main/java/net/leibi/adventofcode2021/day9/Day92021.java
+++ src/main/java/net/leibi/adventofcode2021/day9/Day92021.java
@@ -1,18 +1,110 @@
 package net.leibi.adventofcode2021.day9;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import net.leibi.helpers.InputHelper;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Day92021 {
 
+  static int solveBonus(final String input) {
+
+    int[][] matrix = Day92021.parseInput(input);
+    List<LowPoint> lowpoints = getLowPoints(matrix);
+
+    List<LowPoint> basin = new ArrayList<>();
+    List<Integer> basinSizes = new ArrayList<>();
+    for (LowPoint lowpoint : lowpoints) {
+      basin.clear();
+      basinSizes.add(getBasinForLowPoint(matrix, lowpoint, basin).size());
+    }
+
+    Collections.sort(basinSizes);
+
+    return  basinSizes.get(basinSizes.size()-1) * basinSizes.get(basinSizes.size()-2) * basinSizes.get(basinSizes.size()-3);
+
+  }
+
+  static List<LowPoint> getBasinForLowPoint(
+      int[][] matrix, LowPoint lowPoint, List<LowPoint> basin) {
+    if (!basin.contains(lowPoint)) {
+      basin.add(lowPoint);
+    }
+    List<LowPoint> nextPoints = new ArrayList<>();
+
+    int height = matrix.length;
+    int width = matrix[0].length;
+    // right
+    int currentRow = lowPoint.row + 1;
+    int currentCol = lowPoint.column;
+    while (currentRow < width) {
+      if (reachedANine(matrix, currentCol, currentRow, nextPoints, lowPoint, basin)) {
+        break;
+      }
+      currentRow++;
+    }
+    // left
+    currentRow = lowPoint.row - 1;
+    currentCol = lowPoint.column;
+    while (currentRow >= 0) {
+      if (reachedANine(matrix, currentCol, currentRow, nextPoints, lowPoint, basin)) break;
+      currentRow--;
+    }
+    // up
+    currentRow = lowPoint.row;
+    currentCol = lowPoint.column - 1;
+    while (currentCol >= 0) {
+      if (reachedANine(matrix, currentCol, currentRow, nextPoints, lowPoint, basin)) break;
+      currentCol--;
+    }
+    // down
+    currentCol = lowPoint.column + 1;
+    currentRow = lowPoint.row;
+    while (currentCol < height) {
+      if (reachedANine(matrix, currentCol, currentRow, nextPoints, lowPoint, basin)) break;
+      currentCol++;
+    }
+
+    nextPoints.forEach(nextPoint -> getBasinForLowPoint(matrix, nextPoint, basin));
+    return basin;
+  }
+
+  static List<LowPoint> getLowPoints(int[][] matrix) {
+    List<LowPoint> result = new ArrayList<>();
+
+    int height = matrix.length;
+    int width = matrix[0].length;
+
+    for (int columnCursor = 0; columnCursor < height; columnCursor++) {
+      for (int rowCursor = 0; rowCursor < width; rowCursor++) {
+
+        int up = getUp(matrix, columnCursor, rowCursor);
+        int down = getDown(matrix, height, columnCursor, rowCursor);
+        int left = getLeft(matrix, columnCursor, rowCursor);
+        int right = getRight(matrix, width, columnCursor, rowCursor);
+
+        if (matrix[columnCursor][rowCursor] < Math.min(Math.min(Math.min(up, down), left), right)) {
+          result.add(
+              LowPoint.builder()
+                  .value(matrix[columnCursor][rowCursor])
+                  .column(columnCursor)
+                  .row(rowCursor)
+                  .build());
+        }
+      }
+    }
+    return result;
+  }
+
   static int solve(final String input) {
 
-    List<Integer> lowpoints = getLowPoints(parseInput(input));
-    return lowpoints.size() + lowpoints.stream().mapToInt(i -> i).sum();
+    List<LowPoint> lowpoints = getLowPoints(parseInput(input));
+    return lowpoints.size() + lowpoints.stream().mapToInt(i -> i.value).sum();
   }
 
   static int[][] parseInput(final String input) {
@@ -28,27 +120,51 @@ public class Day92021 {
     return result;
   }
 
-  static List<Integer> getLowPoints(int[][] matrix) {
-
-    List<Integer> result = new ArrayList<>();
-
-    int height = matrix.length;
-    int width = matrix[0].length;
-
-    for (int columnCursor = 0; columnCursor < height; columnCursor++) {
-      for (int rowCursor = 0; rowCursor < width; rowCursor++) {
-
-        int up = getUp(matrix, columnCursor, rowCursor);
-        int down = getDown(matrix, height, columnCursor, rowCursor);
-        int left = getLeft(matrix, columnCursor, rowCursor);
-        int right = getRight(matrix, width, columnCursor, rowCursor);
-
-        if (matrix[columnCursor][rowCursor] < Math.min(Math.min(Math.min(up, down), left), right)) {
-          result.add(matrix[columnCursor][rowCursor]);
-        }
+  private static boolean reachedANine(
+      int[][] matrix,
+      int currentCol,
+      int currentRow,
+      List<LowPoint> nextPoints,
+      LowPoint originalLowPoint,
+      List<LowPoint> basin) {
+    if (matrix[currentCol][currentRow] < 9) {
+      LowPoint lp =
+          LowPoint.builder()
+              .row(currentRow)
+              .column(currentCol)
+              .value(matrix[currentCol][currentRow])
+              .build();
+      if (!nextPoints.contains(lp) && !basin.contains(lp) && lp != originalLowPoint) {
+        nextPoints.add(lp);
       }
+      if (!basin.contains(lp)) {
+        basin.add(lp);
+      }
+    } else return true;
+    return false;
+  }
+
+  @Builder
+  static class LowPoint {
+    int column;
+    int row;
+    int value;
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (!(o instanceof LowPoint lowPoint)) {
+        return false;
+      }
+      return column == lowPoint.column && row == lowPoint.row && value == lowPoint.value;
     }
-    return result;
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(column, row, value);
+    }
   }
 
   private static int getRight(int[][] matrix, int width, int columnCursor, int rowCursor) {
--- src/test/java/net/leibi/adventofcode2021/day9/Day92021Test.java
+++ src/test/java/net/leibi/adventofcode2021/day9/Day92021Test.java
@@ -2,10 +2,38 @@ package net.leibi.adventofcode2021.day9;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
+import net.leibi.adventofcode2021.day9.Day92021.LowPoint;
 import org.junit.jupiter.api.Test;
 
 class Day92021Test {
+
+  @Test
+  void solveBonus() {
+    assertThat(Day92021.solveBonus(Input.SHORT_INPUT)).isEqualTo(1134);
+  }
+
+  @Test
+  void solveBonusLarge() {
+    assertThat(Day92021.solveBonus(Input.LONG_INPUT)).isEqualTo(902880);
+  }
+
+  @Test
+  void getBasinForLowPoint() {
+
+    int[][] matrix = Day92021.parseInput(Input.SHORT_INPUT);
+    List<LowPoint> lowPoints = Day92021.getLowPoints(matrix);
+    List<LowPoint> basin = new ArrayList<>();
+    assertThat(Day92021.getBasinForLowPoint(matrix,lowPoints.get(0), basin)).hasSize(3);//.contains(2,1,3);
+    basin.clear();
+    assertThat(Day92021.getBasinForLowPoint(matrix,lowPoints.get(1),basin)).hasSize(9);//.contains(4,3,2,1,0,4,2,1,2);
+    basin.clear();
+    assertThat(Day92021.getBasinForLowPoint(matrix,lowPoints.get(2),basin)).hasSize(14);//.contains(8,7,8,8,5,6,7,8,8,7,6,7,8);
+    basin.clear();
+    assertThat(Day92021.getBasinForLowPoint(matrix,lowPoints.get(3),basin)).hasSize(9);//.contains(8,6,7,8,6,5,6,7,8);
+
+  }
 
   @Test
   void solve() {
@@ -20,9 +48,11 @@ class Day92021Test {
   @Test
   void getLowPoints() {
     int[][] matrix = Day92021.parseInput(Input.SHORT_INPUT);
-    List<Integer> lowPoints = Day92021.getLowPoints(matrix);
+    List<LowPoint> lowPoints = Day92021.getLowPoints(matrix);
 
-    assertThat(lowPoints).hasSize(4).containsExactly(1, 0, 5, 5);
+    assertThat(lowPoints)
+        .hasSize(4)
+        .anySatisfy(lowPoint -> assertThat(lowPoint.value).isIn(1, 0, 5, 5));
   }
 
   @Test
