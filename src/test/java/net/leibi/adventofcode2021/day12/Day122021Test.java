b74ff3eb25547e77de13d47f345e2509c5935d14
Andreas Leibrock <aleibrock@vwd.com>
2022-12-02 12:35:05 +0100

    disable old test

--- src/test/java/net/leibi/adventofcode2021/day12/Day122021Test.java
+++ src/test/java/net/leibi/adventofcode2021/day12/Day122021Test.java
@@ -4,11 +4,15 @@ import static net.leibi.adventofcode2021.day12.Input.SHORT_INPUT;
 import static net.leibi.adventofcode2021.day12.Input.SHORT_RESULT;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class Day122021Test {
 
+
   @Test
+  @Disabled
   void getAllPaths() {
     assertThat(Day122021.getAllPaths(SHORT_INPUT)).hasSize(10);
   }
