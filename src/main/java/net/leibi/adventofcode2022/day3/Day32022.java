1d3c01ce98a79531656b09d167714c3606a01ed0
Andreas Leibrock <aleibrock@vwd.com>
2022-12-03 15:25:48 +0100

    day3: part 2

--- src/main/java/net/leibi/adventofcode2022/day3/Day32022.java
+++ src/main/java/net/leibi/adventofcode2022/day3/Day32022.java
@@ -1,5 +1,8 @@
 package net.leibi.adventofcode2022.day3;
 
+import static com.google.common.primitives.Ints.max;
+
+import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,17 +18,55 @@ public class Day32022 {
         .reduce(0, Integer::sum);
   }
 
+  static Integer scorePart2(String input) {
+    List<Group> groups = getListOfGroups(input);
+    return groups.stream().map(Group::getCommonItem).map(Day32022::getValueFromItem).reduce(0,Integer::sum);
+  }
+
   static List<Rucksack> getRucksackListFromInput(final String input) {
     return input.lines().map(Rucksack::new).toList();
   }
 
-
-  static Integer getValueFromItem(final String item) {
-    int intValue = (item.charAt(0));
+  static Integer getValueFromItem(final Character item) {
+    int intValue = item;
     if (intValue > 95) {
       return intValue - 96;
     }
     return intValue - 38;
+  }
+
+  static List<Group> getListOfGroups(String input) {
+    return Lists.partition(
+            getRucksackListFromInput(input), 3).stream()
+        .map(l -> new Group(l.get(0), l.get(1), l.get(2))).toList();
+  }
+
+  record Group(Rucksack r1, Rucksack r2, Rucksack r3) {
+
+    public Character getCommonItem() {
+      int l1 = r1.content.length();
+      int l2 = r2.content.length();
+      int l3 = r3.content.length();
+
+      int maxLength = max(l1, l2, l3);
+      Rucksack rMax = null;
+      if(l1 == maxLength)
+        rMax = r1;
+      else if(l2 == maxLength)
+        rMax = r2;
+      else
+        rMax = r3;
+
+
+      for (int i = 0; i < maxLength; i++) {
+        char ch = rMax.content.charAt(i);
+        if (r1.content.indexOf(ch) != -1 && r2.content.indexOf(ch) != -1 && r3.content.indexOf(ch) != -1) {
+          return ch;
+        }
+
+      }
+      return null;
+    }
   }
 
   record Rucksack(String content, String firstCompartment, String secondCompartment) {
@@ -35,12 +76,12 @@ public class Day32022 {
           content.substring((content.length() / 2)));
     }
 
-    List<String> getCommonItems() {
-      var result = new ArrayList<String>();
+    List<Character> getCommonItems() {
+      var result = new ArrayList<Character>();
       for (int i = 0; i < firstCompartment.length(); i++) {
-        char ch = firstCompartment.charAt(i);
-        if (secondCompartment.indexOf(ch) != -1 && !result.contains(String.valueOf(ch))) {
-          result.add(String.valueOf(ch));
+        Character ch = firstCompartment.charAt(i);
+        if (secondCompartment.indexOf(ch) != -1 && !result.contains(ch)) {
+          result.add(ch);
         }
       }
       return result;
--- src/test/java/net/leibi/adventofcode2022/day3/Day32022Test.java
+++ src/test/java/net/leibi/adventofcode2022/day3/Day32022Test.java
@@ -2,10 +2,39 @@ package net.leibi.adventofcode2022.day3;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import net.leibi.adventofcode2022.day3.Day32022.Group;
 import net.leibi.adventofcode2022.day3.Day32022.Rucksack;
 import org.junit.jupiter.api.Test;
 
 class Day32022Test {
+
+
+  @Test
+  void scorePart2() {
+    assertThat(Day32022.scorePart2(Input.small)).isEqualTo(70);
+  }
+
+  @Test
+  void scorePart2Large() {
+    assertThat(Day32022.scorePart2(Input.large)).isEqualTo(2646);
+  }
+
+  @Test
+  void getListOfGroupsSmall() {
+    assertThat(Day32022.getListOfGroups(Input.small)).hasSize(2)
+        .anySatisfy(
+            group -> assertThat(group).isEqualTo(new Group(
+                new Rucksack("vJrwpWtwJgWrhcsFMMfFFhFp"),
+                new Rucksack("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"),
+                new Rucksack("PmmdzqPrVvPwwTWBwg"))
+            ));
+  }
+
+  @Test
+  void getCommonItems() {
+    assertThat(Day32022.getListOfGroups(Input.small).get(0).getCommonItem()).isEqualTo('r');
+    assertThat(Day32022.getListOfGroups(Input.small).get(1).getCommonItem()).isEqualTo('Z');
+  }
 
 
   @Test
@@ -20,10 +49,10 @@ class Day32022Test {
 
   @Test
   void getValueFromItem() {
-    assertThat(Day32022.getValueFromItem("a")).isEqualTo(1);
-    assertThat(Day32022.getValueFromItem("z")).isEqualTo(26);
-    assertThat(Day32022.getValueFromItem("A")).isEqualTo(27);
-    assertThat(Day32022.getValueFromItem("Z")).isEqualTo(52);
+    assertThat(Day32022.getValueFromItem('a')).isEqualTo(1);
+    assertThat(Day32022.getValueFromItem('z')).isEqualTo(26);
+    assertThat(Day32022.getValueFromItem('A')).isEqualTo(27);
+    assertThat(Day32022.getValueFromItem('Z')).isEqualTo(52);
   }
 
   @Test
@@ -54,16 +83,16 @@ class Day32022Test {
     assertThat(r.getCommonItems()).isEmpty();
 
     r = new Rucksack("abac");
-    assertThat(r.getCommonItems()).hasSize(1).containsExactly("a");
+    assertThat(r.getCommonItems()).hasSize(1).containsExactly('a');
 
     r = new Rucksack("abcafg");
-    assertThat(r.getCommonItems()).hasSize(1).containsExactly("a");
+    assertThat(r.getCommonItems()).hasSize(1).containsExactly('a');
 
     r = new Rucksack("abcafc");
-    assertThat(r.getCommonItems()).hasSize(2).containsExactly("a", "c");
+    assertThat(r.getCommonItems()).hasSize(2).containsExactly('a', 'c');
 
     r = new Rucksack("aaaa");
-    assertThat(r.getCommonItems()).hasSize(1).containsExactly("a");
+    assertThat(r.getCommonItems()).hasSize(1).containsExactly('a');
 
   }
 
