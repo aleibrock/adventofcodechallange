f4c296b3198f6975265bf579ee97a369fe9164fb
Andreas Leibrock <aleibrock@vwd.com>
2022-12-04 09:41:53 +0100

    day4: clean up

--- src/main/java/net/leibi/adventofcode2022/day4/Day42022.java
+++ src/main/java/net/leibi/adventofcode2022/day4/Day42022.java
@@ -1,17 +1,18 @@
 package net.leibi.adventofcode2022.day4;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.IntStream;
 import net.leibi.helpers.InputHelper;
 
 public class Day42022 {
 
-  static long getCompleteOverlaps(final String input){
+  static long getCompleteOverlaps(final String input) {
     return getListOfPairs(input).stream().filter(ElfePair::hasCompleteOverlap).count();
   }
 
-  static long getPartialOverlaps(final String input){
+  static long getPartialOverlaps(final String input) {
     return getListOfPairs(input).stream().filter(ElfePair::hasPartialOverlap).count();
   }
 
@@ -23,18 +24,6 @@ public class Day42022 {
 
   record ElfePair(List<Integer> firstSection, List<Integer> secondSection) {
 
-    public boolean hasPartialOverlap() {
-      if(hasCompleteOverlap())
-        return true;
-
-      return firstSection.stream().filter(i -> secondSection.contains(i)).count() > 0;
-
-    }
-
-    Boolean hasCompleteOverlap(){
-      return firstSection.containsAll(secondSection) || secondSection.containsAll(firstSection);
-    }
-
     public ElfePair(String firstSection, String secondSection) {
       this(new ArrayList<>(), new ArrayList<>());
       this.firstSection.addAll(getIntegerListFromSectionString(firstSection));
@@ -42,9 +31,19 @@ public class Day42022 {
 
     }
 
+    public boolean hasPartialOverlap() {
+      return firstSection.stream().anyMatch(secondSection::contains);
+    }
+
+    Boolean hasCompleteOverlap() {
+      return new HashSet<>(firstSection).containsAll(secondSection) ||
+          new HashSet<>(secondSection).containsAll(firstSection);
+    }
+
     private List<Integer> getIntegerListFromSectionString(String sectionString) {
       String[] split = sectionString.split("-");
-      return IntStream.range(Integer.parseInt(split[0]), Integer.parseInt(split[1])+1).boxed().toList();
+      return IntStream.range(Integer.parseInt(split[0]), Integer.parseInt(split[1]) + 1).boxed()
+          .toList();
     }
   }
 
