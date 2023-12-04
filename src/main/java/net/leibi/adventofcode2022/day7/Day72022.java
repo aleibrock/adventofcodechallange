6d9439525bde241ca67d700cb7912d517f04c567
Andreas Leibrock <git@leibi.net>
2023-12-04 11:13:26 +0100

    whatever

--- src/main/java/net/leibi/adventofcode2022/day7/Day72022.java
+++ src/main/java/net/leibi/adventofcode2022/day7/Day72022.java
@@ -1,2 +1,75 @@
-package net.leibi.adventofcode2022.day7;public class Day72022 {
+package net.leibi.adventofcode2022.day7;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import net.leibi.helpers.InputHelper;
+
+public class Day72022 {
+
+  static final Integer SIZELIMIT = 100000;
+
+  public static Integer getSmallDirectoriesSize(String s) {
+    var output = InputHelper.getRowListFromInput(s);
+
+    var root = getDirectoryHierachy(output.subList(1, output.size()), null, "/");
+
+    return root.directoryList.stream().map(Directory::totalSize)
+        .filter(directorySize -> directorySize < SIZELIMIT)
+        .reduce(0, Integer::sum);
+  }
+
+  static Directory getDirectoryHierachy(List<String> output, Directory parentDirectory,
+      String newDirectoryName) {
+    if (newDirectoryName.equals("..")) {
+      return parentDirectory;
+    }
+    var currentDirectory = new Directory(newDirectoryName);
+    var addedDirectories = new ArrayList<Directory>();
+    for (int i = 0; i < output.size(); i++) {
+      String outputRow = output.get(i);
+      String[] splittedOutput = outputRow.split(" ");
+      if (splittedOutput[0].equals("$") && splittedOutput[1].equals("cd") && !splittedOutput[2].equals("..")) {
+        addedDirectories.add(
+            getDirectoryHierachy(output.subList(i + 1, output.size()), currentDirectory,
+                splittedOutput[2]));
+        break;
+      } else if (isFile(splittedOutput)) {
+        currentDirectory.files.add(new File(splittedOutput[1], Integer.valueOf(splittedOutput[0])));
+      } else if (splittedOutput[0].equals("$") && splittedOutput[1].equals("cd") && splittedOutput[2].equals("..")){
+        currentDirectory.directoryList.addAll(addedDirectories);
+        return currentDirectory;
+      }
+    }
+    currentDirectory.directoryList.addAll(addedDirectories);
+    return currentDirectory;
+  }
+
+  private static boolean isFile(String[] splittedOutput) {
+    return splittedOutput[0].matches("\\d+");
+  }
+
+
+  public record File(String name, Integer size) {
+
+  }
+
+  record Directory(String name, List<Directory> directoryList, List<File> files) {
+
+    Directory(String name) {
+      this(name, new ArrayList<>(), new ArrayList<>());
+    }
+
+    public Integer totalSize() {
+      return files.stream().map(File::size).reduce(0, Integer::sum)
+          + directoryList.stream().map(Directory::totalSize).reduce(0, Integer::sum);
+    }
+
+    @Override
+    public String toString() {
+      return "Directory{" +
+          "name='" + name + '\'' +
+          '}';
+    }
+  }
 }
--- src/test/java/net/leibi/adventofcode2022/day7/Day72022Test.java
+++ src/test/java/net/leibi/adventofcode2022/day7/Day72022Test.java
@@ -1,4 +1,127 @@
-import static org.junit.jupiter.api.Assertions.*;
+package net.leibi.adventofcode2022.day7;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import net.leibi.adventofcode2022.day7.Day72022.Directory;
+import net.leibi.adventofcode2022.day7.Day72022.File;
+import net.leibi.helpers.InputHelper;
+import org.junit.jupiter.api.Test;
+
 class Day72022Test {
-  
+
+  @Test
+  void getDirectoryHierachy() {
+
+    String input = """
+        $ ls
+        dir a
+        14848514 b.txt
+        8504156 c.dat
+        dir d
+          """;
+
+    Directory directoryHierachy = Day72022.getDirectoryHierachy(
+        InputHelper.getRowListFromInput(input), null, "/");
+    assertThat(directoryHierachy).isNotNull();
+    assertThat(directoryHierachy.directoryList()).isEmpty();
+    assertThat(directoryHierachy.files()).hasSize(2);
+
+
+
+  }
+
+  @Test
+  void getDirectoryHierachyWithFilesInA() {
+
+    String input = """
+      $ ls
+      dir a
+      14848514 b.txt
+      8504156 c.dat
+      dir d
+      $ cd a
+      $ ls
+      dir e
+      29116 f
+      2557 g
+      62596 h.lst
+      """;
+
+    Directory directoryHierachy = Day72022.getDirectoryHierachy(
+        InputHelper.getRowListFromInput(input), null, "/");
+    assertThat(directoryHierachy).isNotNull();
+    assertThat(directoryHierachy.directoryList()).hasSize(1);
+    assertThat(directoryHierachy.directoryList().get(0).files()).hasSize(2);
+    assertThat(directoryHierachy.files()).hasSize(2);
+
+  }
+
+  @Test
+  void getDirectoryHierachyWithFilesInAAndE() {
+
+    String input = """
+      $ ls
+      dir a
+      14848514 b.txt
+      8504156 c.dat
+      dir d
+      $ cd a
+      $ ls
+      dir e
+      29116 f
+      2557 g
+      62596 h.lst
+      $ cd e
+      $ ls
+      584 i
+      """;
+
+    Directory directoryHierachy = Day72022.getDirectoryHierachy(
+        InputHelper.getRowListFromInput(input), null, "/");
+    assertThat(directoryHierachy).isNotNull();
+    assertThat(directoryHierachy.directoryList()).hasSize(1);
+    assertThat(directoryHierachy.directoryList().get(0).name()).isEqualTo("a");
+    assertThat(directoryHierachy.directoryList().get(0).files()).hasSize(3);
+    assertThat(directoryHierachy.directoryList().get(0).directoryList()).hasSize(1);
+    assertThat(directoryHierachy.directoryList().get(0).directoryList().get(0).name()).isEqualTo("e");
+    assertThat(directoryHierachy.directoryList().get(0).directoryList().get(0).files()).hasSize(1);
+    assertThat(directoryHierachy.files()).hasSize(2);
+  }
+
+
+  @Test
+  void getDirectoryHierachySmall() {
+
+    List<String> rowListFromInput = InputHelper.getRowListFromInput(Input.small);
+    Directory directoryHierachy = Day72022.getDirectoryHierachy(
+        rowListFromInput.subList(1,rowListFromInput.size()), null, "/");
+    assertThat(directoryHierachy).isNotNull();
+    assertThat(directoryHierachy.directoryList()).hasSize(1);
+    assertThat(directoryHierachy.directoryList().get(0).name()).isEqualTo("a");
+    assertThat(directoryHierachy.directoryList().get(0).files()).hasSize(3);
+    assertThat(directoryHierachy.directoryList().get(0).directoryList()).hasSize(1);
+    assertThat(directoryHierachy.directoryList().get(0).directoryList().get(0).name()).isEqualTo("e");
+    assertThat(directoryHierachy.directoryList().get(0).directoryList().get(0).files()).hasSize(1);
+    assertThat(directoryHierachy.files()).hasSize(2);
+  }
+
+
+  @Test
+  void getSmallDirectoriesSize() {
+    assertThat(Day72022.getSmallDirectoriesSize(Input.small)).isEqualTo(95437);
+  }
+
+  @Test
+  void TestDirectoryTotalSize() {
+
+    var dir = new Directory("a", new ArrayList<>(), new ArrayList<>());
+    dir.directoryList().add(new Directory("b", new ArrayList<>(), List.of(new File("f_a", 10))));
+    dir.files().add(new File("f_b", 100));
+    dir.files().add(new File("f_c", 5));
+    assertThat(dir.totalSize()).isEqualTo(115);
+  }
+
+
 }
