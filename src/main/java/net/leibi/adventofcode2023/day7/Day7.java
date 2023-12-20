a1031e78b85cdefbfc05b5790cbd6c6d6e6dad8d
Andreas Leibrock <git@leibi.net>
2023-12-20 21:11:32 +0100

    Day7 part 1 small

--- src/main/java/net/leibi/adventofcode2023/day7/Day7.java
+++ src/main/java/net/leibi/adventofcode2023/day7/Day7.java
@@ -1,17 +1,60 @@
 package net.leibi.adventofcode2023.day7;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static net.leibi.adventofcode2023.day7.Day7.Cards.WIN.*;
+
+@Slf4j
 public class Day7 {
-    List<Character> cardOrder = List.of('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2');
+    static List<Character> cardOrder = List.of('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2');
 
     public Long getTotalWinnings(String input) {
-        return 6440L;
+        var hands = getHands(input);
+        log.info("hands: {}", hands);
+        var sortedHands = new ArrayList<>(hands);
+        sortedHands.sort(Hand::isBetter);
+        log.info("sorted hands: {}", sortedHands);
+        return IntStream.range(0, sortedHands.size())
+                .mapToLong(rank -> {
+                    log.info("rank: {}, value {}, result: {}", rank, sortedHands.get(rank).Bid, sortedHands.get(rank).Bid * (rank + 1));
+                    return sortedHands.get(rank).Bid * (rank + 1);
+                })
+                .sum();
     }
 
     public List<Hand> getHands(String input) {
         return input.lines().map(this::getHand).toList();
+    }
+
+    static int isCardHigher(Character c1, Character c2) {
+        return Integer.compare(cardOrder.indexOf(c2), cardOrder.indexOf(c1));
+    }
+
+    /*
+    static Character getHighestCard(Cards cards) {
+        var thisMap = cards.stream().collect(Collectors.groupingBy(Character::charValue, Collectors.counting()));
+        var sorted = thisMap.entrySet().stream().sorted((e1, e2) -> isCardHigher(e2.getKey(), e1.getKey()));
+        return sorted.findFirst().get().getKey();
+    }
+
+     */
+
+    static List<Character> sortCards(Cards cards) {
+        var thisMap = cards.stream().collect(Collectors.groupingBy(Character::charValue, Collectors.counting()));
+        return thisMap.entrySet().stream()
+                .sorted((e1, e2) -> isCardHigher(e2.getKey(), e1.getKey()))
+                .map(Map.Entry::getKey).distinct()
+                .toList();
+    }
+
+    private static List<Character> sortCards(List<Character> characters) {
+        return characters.stream().sorted((c1, c2) -> -1 * Day7.isCardHigher(c1, c2)).toList();
     }
 
 
@@ -22,9 +65,32 @@ public class Day7 {
 
     public record Hand(Cards cards, Long Bid) {
 
+        public int isBetter(Hand o2) {
+            var myRank = cards.getWin().ordinal();
+            var otherRank = o2.cards.getWin().ordinal();
+            var compare = Integer.compare(otherRank, myRank);
+            if (0 != compare) {
+                return compare;
+            }
+            // determine highest card of the winning bunch
+            var sorted = new ArrayList<>(sortCards(cards.getWinningCards()));
+            var otherSorted = new ArrayList<>(sortCards(o2.cards.getWinningCards()));
+            assert (sorted.size() == otherSorted.size());
+            while (sorted.getFirst().equals(otherSorted.getFirst())) {
+                sorted.removeFirst();
+                otherSorted.removeFirst();
+            }
+
+            return isCardHigher(sorted.getFirst(), otherSorted.getFirst());
+
+
+        }
+
+
     }
 
     public static class Cards extends ArrayList<Character> {
+        ;
 
         public Cards(String cardsAsString) {
             super(cardsAsString.length());
@@ -32,5 +98,56 @@ public class Day7 {
                 add(cardsAsString.toCharArray()[i]);
             }
         }
+
+        WIN getWin() {
+            var collectionMap = this.stream().collect(Collectors.groupingBy(Character::charValue, Collectors.counting()));
+            return switch (collectionMap.keySet().size()) {
+                case 1 -> FIVE_OAK;
+                case 2 -> {
+                    // full house or four of a kind
+                    if (collectionMap.values().stream().anyMatch(size -> size.compareTo(2L) == 0))
+                        yield FULL_HOUSE;
+                    yield FOUR_OAK;
+                }
+                case 3 -> {
+                    if (collectionMap.values().stream().anyMatch(size -> size.compareTo(3L) == 0))
+                        yield THREE_OAK;
+                    yield TWO_PAIR;
+                }
+                case 4 -> ONE_PAIR;
+                case 5 -> HIGH;
+                default -> throw new RuntimeException("what?");
+            };
+        }
+
+        List<Character> getWinningCards() {
+            var collectionMap = this.stream().collect(Collectors.groupingBy(Character::charValue, Collectors.counting()));
+            return switch (getWin()) {
+                case FIVE_OAK -> collectionMap.keySet().stream().distinct().toList();
+                case FOUR_OAK ->
+                        collectionMap.entrySet().stream().filter(e -> e.getValue().equals(4L)).map(Map.Entry::getKey).toList();
+                case FULL_HOUSE -> collectionMap.keySet().stream().toList();
+                case THREE_OAK ->
+                        collectionMap.entrySet().stream().filter(e -> e.getValue().equals(3L)).map(Map.Entry::getKey).toList();
+                case TWO_PAIR ->
+                        collectionMap.entrySet().stream().filter(e -> e.getValue().equals(2L)).map(Map.Entry::getKey).toList();
+                case ONE_PAIR ->
+                        collectionMap.entrySet().stream().filter(e -> e.getValue().equals(2L)).map(Map.Entry::getKey).toList();
+                case HIGH -> collectionMap.keySet().stream().toList();
+            };
+        }
+
+        enum WIN {
+            FIVE_OAK,
+            FOUR_OAK,
+            FULL_HOUSE,
+            THREE_OAK,
+            TWO_PAIR,
+            ONE_PAIR,
+            HIGH
+
+        }
+
+
     }
 }
--- src/test/java/net/leibi/adventofcode2023/day7/Day7Test.java
+++ src/test/java/net/leibi/adventofcode2023/day7/Day7Test.java
@@ -2,6 +2,8 @@ package net.leibi.adventofcode2023.day7;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Day7Test {
@@ -16,7 +18,53 @@ class Day7Test {
     @Test
     void getHands() {
         Day7.Cards cards = new Day7.Cards("32T3K");
-        assertThat(day7.getHands(Input.SMALL)).hasSize(5).contains(new Day7.Hand(cards,765L));
+        assertThat(day7.getHands(Input.SMALL)).hasSize(5).contains(new Day7.Hand(cards, 765L));
     }
+
+    @Test
+    void testWinngings() {
+        assertThat(new Day7.Cards("KKKKK").getWin()).isEqualTo(Day7.Cards.WIN.FIVE_OAK);
+        assertThat(new Day7.Cards("AKKKK").getWin()).isEqualTo(Day7.Cards.WIN.FOUR_OAK);
+        assertThat(new Day7.Cards("AAKKK").getWin()).isEqualTo(Day7.Cards.WIN.FULL_HOUSE);
+        assertThat(new Day7.Cards("AKAKK").getWin()).isEqualTo(Day7.Cards.WIN.FULL_HOUSE);
+        assertThat(new Day7.Cards("AATKK").getWin()).isEqualTo(Day7.Cards.WIN.TWO_PAIR);
+        assertThat(new Day7.Cards("A2A3K").getWin()).isEqualTo(Day7.Cards.WIN.ONE_PAIR);
+        assertThat(new Day7.Cards("23456").getWin()).isEqualTo(Day7.Cards.WIN.HIGH);
+
+
+    }
+
+    @Test
+    void testWinningCards() {
+        assertThat(new Day7.Cards("KKKKK").getWinningCards()).isEqualTo(List.of('K'));
+        assertThat(new Day7.Cards("AKKKK").getWinningCards()).isEqualTo(List.of('K'));
+        assertThat(new Day7.Cards("AAKKK").getWinningCards()).isEqualTo(List.of('A', 'K'));
+        assertThat(new Day7.Cards("AKAKK").getWinningCards()).isEqualTo(List.of('A', 'K'));
+        assertThat(new Day7.Cards("AATKK").getWinningCards()).isEqualTo(List.of('A', 'K'));
+        assertThat(new Day7.Cards("A2A3K").getWinningCards()).isEqualTo(List.of('A'));
+        assertThat(new Day7.Cards("23456").getWinningCards()).isEqualTo(List.of('2', '3', '4', '5', '6'));
+
+
+    }
+
+    @Test
+    void testHigherCard() {
+        assertThat(Day7.isCardHigher('A', '6')).isEqualTo(1);
+        assertThat(Day7.isCardHigher('K', 'A')).isEqualTo(-1);
+        assertThat(Day7.isCardHigher('K', 'K')).isZero();
+
+    }
+
+    /*
+    @Test
+    void testHighestCard(){
+       assertThat(Day7.getHighestCard(new Day7.Cards("AAKKK"))).isEqualTo('A');
+        assertThat(Day7.getHighestCard(new Day7.Cards("23456"))).isEqualTo('6');
+        assertThat(Day7.getHighestCard(new Day7.Cards("AATKK"))).isEqualTo('A');
+
+    }
+
+     */
+
 
 }
