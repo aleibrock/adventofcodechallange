ed8279ac0bf96616048be421dfa7cb0e6c89a1a6
Andreas Leibrock <git@leibi.net>
2023-12-30 10:24:15 +0100

    Day9 part1

--- src/test/java/net/leibi/adventofcode2023/day9/Day9Test.java
+++ src/test/java/net/leibi/adventofcode2023/day9/Day9Test.java
@@ -9,18 +9,22 @@ class Day9Test {
     Day9 day9 = new Day9();
 
     @Test
-    void testSmall(){
+    void testSmall() {
         assertThat(day9.getSumOfExtrapolatedValues(Input.SMALL)).isEqualTo(114);
+    }
+
+    @Test
+    void testBig() {
+        assertThat(day9.getSumOfExtrapolatedValues(Input.BIG)).isEqualTo(1725987467L);
     }
 
 
     @Test
-    void testGetExtrapolatedValuesFromHistory(){
+    void testGetExtrapolatedValuesFromHistory() {
         assertThat(day9.getExtrapolatedValuesFromHistory("0 3 6 9 12 15")).isEqualTo(18);
         assertThat(day9.getExtrapolatedValuesFromHistory("1 3 6 10 15 21")).isEqualTo(28);
         assertThat(day9.getExtrapolatedValuesFromHistory("10 13 16 21 30 45")).isEqualTo(68);
     }
-
 
 
 }
