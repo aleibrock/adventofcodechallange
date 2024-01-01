b4aaacebd949c8f900b0cfd2a87c6e0bcc30197f
Andreas Leibrock <git@leibi.net>
2024-01-01 10:56:59 +0100

    Day11 part2 intermediate

--- src/main/java/net/leibi/adventofcode2023/day11/Day11.java
+++ src/main/java/net/leibi/adventofcode2023/day11/Day11.java
@@ -1,5 +1,6 @@
 package net.leibi.adventofcode2023.day11;
 
+import lombok.extern.slf4j.Slf4j;
 import net.leibi.helpers.InputHelper;
 
 import java.util.ArrayList;
@@ -7,20 +8,51 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class Day11 {
 
     public char[][] getExpandedUniverse(String small) {
-
-        var expandedRows = small.lines().map(this::getExpandedRow).collect(Collectors.joining());
-        return getExpandedColumns(expandedRows);
+        var expandedRows = small.lines().map(row -> getExpandedRow(row, 2)).collect(Collectors.joining());
+        return getExpandedColumns(expandedRows, 2);
     }
 
-    public Integer getSumOfShortestPaths(String small) {
-
-        return getGalaxyPairs(getGalaxiesFromUniverse(getExpandedUniverse(small)))
+    public Long getSumOfShortestPaths(String small) {
+        log.info("Start");
+        var expandedUniverse = getExpandedUniverse(small);
+        log.info("got the universe");
+        var galaxiesFromUniverse = getGalaxiesFromUniverse(expandedUniverse);
+        log.info("got the galaxies ({})", galaxiesFromUniverse.size());
+        var galaxyPairs = getGalaxyPairs(galaxiesFromUniverse);
+        log.info("got the galaxypairs ({})", galaxyPairs.size());
+        return galaxyPairs
                 .stream()
-                .mapToInt(GalaxyPair::getDistance)
+                .mapToLong(GalaxyPair::getDistance)
                 .sum();
+
+    }
+
+    public Long getSumOfShortestPathsPart2(String small, Long factor) {
+        log.info("Start");
+        var galaxiesFromUniverse = getGalaxiesFromUniverse(small);
+
+        var matrix = InputHelper.getCharMatrixFromInput(small);
+        var colList = getColLost(matrix);
+        var rowList = getRowList(matrix);
+
+        var galaxiesFromExpandedUniverse = getGalaxiesFromExpandedUniverse(galaxiesFromUniverse, factor, colList, rowList);
+
+        log.info("got the galaxies ({})", galaxiesFromExpandedUniverse.size());
+        var galaxyPairs = getGalaxyPairs(galaxiesFromExpandedUniverse);
+        log.info("got the galaxypairs ({})", galaxyPairs.size());
+        return galaxyPairs
+                .stream()
+                .mapToLong(GalaxyPair::getDistance)
+                .sum();
+
+    }
+
+    List<Galaxy> getGalaxiesFromExpandedUniverse(List<Galaxy> galaxiesFromUniverse, Long factor, List<Long> rowIndexList, List<Long> columnIndexList) {
+        return galaxiesFromUniverse.stream().map(galaxy -> getExpandedGalaxy(galaxy, factor, rowIndexList, columnIndexList)).toList();
 
     }
 
@@ -56,14 +88,52 @@ public class Day11 {
 
     }
 
-    private char[][] getExpandedColumns(String expandedRows) {
+    Galaxy getExpandedGalaxy(Galaxy galaxy, Long factor, List<Long> rowIndexList, List<Long> columnIndexList) {
+        var newX = getNewCoordinate(galaxy.x, factor, rowIndexList);
+        var newY = getNewCoordinate(galaxy.y, factor, columnIndexList);
+
+        return new Galaxy(newX, newY);
+
+
+    }
+
+    private Long getNewCoordinate(long coordinate, Long factor, List<Long> indexList) {
+        // how can we get from the old coordinate to the new coordinate
+        // with the columnIndex we know how many expansions we have before this
+
+        var count = indexList.stream().filter(idx -> idx < coordinate).count();
+
+        var expansion = factor * count;
+
+        return coordinate + expansion;
+
+    }
+
+    List<Galaxy> getGalaxiesFromUniverse(String small) {
+        var matrix = InputHelper.getCharMatrixFromInput(small);
+        return getGalaxiesFromUniverse(matrix);
+    }
+
+    List<Long> getColLost(char[][] matrix) {
+        var result = new ArrayList<Long>();
+        for (int i = 0; i < matrix.length; i++) {
+            if (!String.valueOf(matrix[i]).contains("#")) {
+                result.add((long) i);
+            }
+        }
+        return result;
+    }
+
+    private char[][] getExpandedColumns(String expandedRows, int repeatCount) {
         var array = InputHelper.getCharMatrixFromInput(expandedRows);
         var width = array[0].length;
 
-        var indexList = getIndexList(width, array);
+        var indexList = getRowList(array);
+
+        int columnCount = repeatCount - 1;
 
         //build new aray
-        var newWidth = width + indexList.size();
+        var newWidth = width + (columnCount * indexList.size());
         var chars = new char[array.length][newWidth];
         for (int i = 0; i < array.length; i++) {
             char[] currentRow = array[i];
@@ -71,8 +141,10 @@ public class Day11 {
             for (int j = 0, nj = 0; j < currentRow.length; nj++, j++) {
                 newRow[nj] = currentRow[j];
                 if (indexList.contains(j)) {
-                    newRow[nj + 1] = '.';
-                    nj++;
+                    for (int i1 = 0; i1 < columnCount; i1++) {
+                        newRow[nj + 1] = '.';
+                        nj++;
+                    }
                 }
             }
             chars[i] = newRow;
@@ -80,9 +152,10 @@ public class Day11 {
         return chars;
     }
 
-    private ArrayList<Integer> getIndexList(int width, char[][] array) {
-        var indexList = new ArrayList<Integer>();
-        for (int i = 0; i < width; i++) {
+    ArrayList<Long> getRowList(char[][] array) {
+        int width = array[0].length;
+        var indexList = new ArrayList<Long>();
+        for (long i = 0; i < width; i++) {
             Character[] col = getColumn(array, i);
             if (Arrays.stream(col).noneMatch(c -> c.equals('#'))) {
                 indexList.add(i);
@@ -91,29 +164,31 @@ public class Day11 {
         return indexList;
     }
 
-    private Character[] getColumn(char[][] array, int index) {
+    private Character[] getColumn(char[][] array, long index) {
 
         Character[] result = new Character[array.length];
         for (int i = 0; i < array.length; i++) {
-            result[i] = array[i][index];
+            result[i] = array[i][(int) index];
         }
         return result;
     }
 
-    private String getExpandedRow(String string) {
+    private String getExpandedRow(String string, int repeatCount) {
 
-        if (string.contains("#")) return string + "\n";
-        return string + "\n" + string + "\n";
+        var newString = string + "\n";
+
+        if (string.contains("#")) return newString;
+        return newString.repeat(repeatCount);
     }
 
     record GalaxyPair(Galaxy galaxy1, Galaxy galaxy2) {
-        public Integer getDistance() {
+        public Long getDistance() {
             var xDist = Math.abs(galaxy1.x - galaxy2.x);
             var yDist = Math.abs(galaxy1.y - galaxy2.y);
             return xDist + yDist;
         }
     }
 
-    record Galaxy(int x, int y) {
+    record Galaxy(long x, long y) {
     }
 }
--- src/test/java/net/leibi/adventofcode2023/day11/Day11Test.java
+++ src/test/java/net/leibi/adventofcode2023/day11/Day11Test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 class Day11Test {
 
     public static final Day11 day11 = new Day11();
@@ -64,9 +65,60 @@ class Day11Test {
         assertThat(day11.getSumOfShortestPaths(Input.SMALL)).isEqualTo(374);
     }
 
+
+    @Test
+    void testGalaxyGeneration() {
+        var galaxiesFromUniverse = day11.getGalaxiesFromUniverse(day11.getExpandedUniverse(Input.SMALL));
+
+        var galaxiesFromOriginalUniverse = day11.getGalaxiesFromUniverse(Input.SMALL);
+
+        var matrix = InputHelper.getCharMatrixFromInput(Input.SMALL);
+        var rowIndexList = day11.getColLost(matrix);
+        var columnIndexList = day11.getRowList(matrix);
+        var galaxiesFromExpandedUniverse = day11.getGalaxiesFromExpandedUniverse(galaxiesFromOriginalUniverse, 1L, rowIndexList, columnIndexList);
+
+        assertThat(galaxiesFromExpandedUniverse).isEqualTo(galaxiesFromUniverse);
+
+
+    }
+
+    @Test
+    void testGetIndexList() {
+        var matrix = InputHelper.getCharMatrixFromInput(Input.SMALL);
+        var rowIndexList = day11.getColLost(matrix);
+        var columnIndexList = day11.getRowList(matrix);
+        assertThat(rowIndexList).contains(3L, 7L);
+        assertThat(columnIndexList).contains(2L, 5L, 8L);
+
+    }
+
+    @Test
+    void testGalaxyExpansion() {
+        var matrix = InputHelper.getCharMatrixFromInput(Input.SMALL);
+        var rowIndexList = day11.getColLost(matrix);
+        var columnIndexList = day11.getRowList(matrix);
+        assertThat(day11.getExpandedGalaxy(new Day11.Galaxy(0, 3), 1L, rowIndexList, columnIndexList)).isEqualTo(new Day11.Galaxy(0, 4));
+
+
+    }
+
+    @Test
+    void testSumOfShortestPathsPart2() {
+
+        assertThat(day11.getSumOfShortestPathsPart2(Input.SMALL, 1L)).isEqualTo(374);
+        assertThat(day11.getSumOfShortestPathsPart2(Input.SMALL, 9L)).isEqualTo(1030);
+        assertThat(day11.getSumOfShortestPathsPart2(Input.SMALL, 99L)).isEqualTo(8410);
+        assertThat(day11.getSumOfShortestPathsPart2(Input.SMALL, 999999L)).isEqualTo(82000210L); //wrong
+    }
+
     @Test
     void testSumOfShortestPathsBig() {
         assertThat(day11.getSumOfShortestPaths(Input.BIG)).isEqualTo(9565386);
+    }
+
+    @Test
+    void testSumOfShortestPathsBigNewAlgo() {
+        assertThat(day11.getSumOfShortestPathsPart2(Input.BIG, 1L)).isEqualTo(9565386);
     }
 
 }
