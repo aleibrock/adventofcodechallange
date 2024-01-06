43bb2df6c7cc064b4b0ab84a7d0f29873d99b265
Andreas Leibrock <git@leibi.net>
2024-01-06 08:54:10 +0100

    Day14 cycles

--- src/main/java/net/leibi/adventofcode2023/day14/Day14.java
+++ src/main/java/net/leibi/adventofcode2023/day14/Day14.java
@@ -1,15 +1,28 @@
 package net.leibi.adventofcode2023.day14;
 
+import lombok.extern.slf4j.Slf4j;
 import net.leibi.helpers.InputHelper;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+
+@Slf4j
 public class Day14 {
     public String tilt(String input) {
 
         final var charMatrixFromInput = InputHelper.getCharMatrixFromInput(input);
-        tiltMirrors(charMatrixFromInput);
+        tiltMirrors(charMatrixFromInput, Directions.NORTH);
 
+        return getStringFromCharArray(charMatrixFromInput);
+    }
+
+    public String cycle(String input, long times) {
+        final var charMatrixFromInput = InputHelper.getCharMatrixFromInput(input);
+        for (long i = 0; i < times; i++) {
+            if (i % 1_000_000 == 0)
+                log.info("Cycle # {}", i);
+            cycleMirrors(charMatrixFromInput);
+        }
         return getStringFromCharArray(charMatrixFromInput);
     }
 
@@ -33,7 +46,51 @@ public class Day14 {
         return sumLoad(tilt(small));
     }
 
-    static void tiltMirrors(char[][] charMatrixFromInput) {
+    public long part2(String small) {
+        return sumLoad(cycle(small, 1_000_000_000));
+    }
+
+    static void tiltMirrors(char[][] charMatrixFromInput, Directions direction) {
+        switch (direction) {
+            case NORTH -> tiltNorth(charMatrixFromInput);
+            case EAST -> tiltEast(charMatrixFromInput);
+            case WEST -> tiltWest(charMatrixFromInput);
+            case SOUTH -> tiltSouth(charMatrixFromInput);
+        }
+    }
+
+    static void tiltSouth(char[][] charMatrixFromInput) {
+        for (int row = charMatrixFromInput.length - 1; row >= 0; row--) {
+            char[] currentRow = charMatrixFromInput[row];
+            for (int col = 0; col < currentRow.length; col++) {
+                // move the current item as high as it gets
+                char node = charMatrixFromInput[row][col];
+                if (node == 'O') {
+                    charMatrixFromInput[row][col] = '.';
+                    int tmpRow = row + 1;
+                    while (tmpRow < charMatrixFromInput.length && charMatrixFromInput[tmpRow][col] == '.') {
+                        tmpRow++;
+                    }
+                    tmpRow -= 1;
+                    charMatrixFromInput[tmpRow][col] = 'O';
+                }
+            }
+        }
+    }
+
+    static void tiltEast(char[][] charMatrixFromInput) {
+        for (int row = 0; row < charMatrixFromInput.length; row++) {
+            moveRight(charMatrixFromInput, row);
+        }
+    }
+
+    static void tiltWest(char[][] charMatrixFromInput) {
+        for (int row = 0; row < charMatrixFromInput.length; row++) {
+            moveLeft(charMatrixFromInput, row);
+        }
+    }
+
+    static void tiltNorth(char[][] charMatrixFromInput) {
         for (int row = 1; row < charMatrixFromInput.length; row++) {
             char[] currentRow = charMatrixFromInput[row];
             for (int col = 0; col < currentRow.length; col++) {
@@ -46,7 +103,7 @@ public class Day14 {
                         tmpRow--;
                     }
                     if (tmpRow != row) {
-                        tmpRow +=1;
+                        tmpRow += 1;
                         charMatrixFromInput[tmpRow][col] = 'O';
                     }
                 }
@@ -54,12 +111,64 @@ public class Day14 {
         }
     }
 
-    private String getStringFromCharArray(char[][] charMatrixFromInput) {
+    static String getStringFromCharArray(char[][] charMatrixFromInput) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < charMatrixFromInput.length; i++) {
             sb.append(new String(charMatrixFromInput[i]));
             sb.append("\n");
         }
         return sb.toString();
+    }
+
+    static void moveLeft(char[][] charMatrixFromInput, int row) {
+        char[] currentRow = charMatrixFromInput[row];
+        for (int col = 0; col < currentRow.length; col++) {
+            // move the left
+            char node = currentRow[col];
+            if (node == 'O') {
+                currentRow[col] = '.';
+                var tmpCol = col - 1;
+                while (tmpCol >= 0 && currentRow[tmpCol] == '.') {
+                    tmpCol--;
+                }
+                if (tmpCol != col) {
+                    tmpCol += 1;
+                    charMatrixFromInput[row][tmpCol] = 'O';
+                }
+            }
+        }
+    }
+
+    static void moveRight(char[][] charMatrixFromInput, int row) {
+        char[] currentRow = charMatrixFromInput[row];
+        for (int col = currentRow.length - 1; col >= 0; col--) {
+            // move the left
+            char node = currentRow[col];
+            if (node == 'O') {
+                currentRow[col] = '.';
+                var tmpCol = col + 1;
+                while (tmpCol < currentRow.length && currentRow[tmpCol] == '.') {
+                    tmpCol++;
+                }
+                if (tmpCol != col) {
+                    tmpCol -= 1;
+                    charMatrixFromInput[row][tmpCol] = 'O';
+                }
+            }
+        }
+    }
+
+    private void cycleMirrors(char[][] charMatrixFromInput) {
+        tiltMirrors(charMatrixFromInput, Directions.NORTH);
+        tiltMirrors(charMatrixFromInput, Directions.WEST);
+        tiltMirrors(charMatrixFromInput, Directions.SOUTH);
+        tiltMirrors(charMatrixFromInput, Directions.EAST);
+    }
+
+    enum Directions {
+        NORTH,
+        WEST,
+        SOUTH,
+        EAST
     }
 }
--- src/test/java/net/leibi/adventofcode2023/day14/Day14Test.java
+++ src/test/java/net/leibi/adventofcode2023/day14/Day14Test.java
@@ -1,5 +1,7 @@
 package net.leibi.adventofcode2023.day14;
 
+import net.leibi.helpers.InputHelper;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,10 +23,175 @@ class Day14Test {
                 OOO
                 """))
                 .isEqualTo("""
-                O#O
-                .OO
+                        O#O
+                        .OO
+                        ...
+                        """);
+    }
+
+    @Test
+    void testCycleSmall() {
+        assertThat(day14.cycle("""
+                .#O
                 ...
+                OOO
+                """, 1))
+                .isEqualTo("""
+                        .#.
+                        ..O
+                        OOO
+                        """);
+    }
+
+
+    @Test
+    void testSouth() {
+
+        String input = """
+                .#O
+                ...
+                OOO
+                """;
+
+        var c = InputHelper.getCharMatrixFromInput(input);
+        Day14.tiltSouth(c);
+        var res = Day14.getStringFromCharArray(c);
+
+        assertThat(res)
+                .isEqualTo("""
+                        .#.
+                        ..O
+                        OOO
+                        """);
+    }
+
+    @Test
+    void testSouth2() {
+
+        String input = """
+                .#O
+                OOO
+                O..
+                """;
+
+        var c = InputHelper.getCharMatrixFromInput(input);
+        Day14.tiltSouth(c);
+        var res = Day14.getStringFromCharArray(c);
+
+        assertThat(res)
+                .isEqualTo("""
+                        .#.
+                        O.O
+                        OOO
+                        """);
+    }
+
+    @Test
+    void testWest() {
+
+        String input = """
+                .#O
+                ..O
+                OOO
+                """;
+
+        var c = InputHelper.getCharMatrixFromInput(input);
+        Day14.tiltWest(c);
+        var res = Day14.getStringFromCharArray(c);
+
+        assertThat(res)
+                .isEqualTo("""
+                        .#O
+                        O..
+                        OOO
+                        """);
+    }
+
+    @Test
+    void testWest2() {
+
+        String input = """
+                .#O
+                ..O
+                .OO
+                """;
+
+        var c = InputHelper.getCharMatrixFromInput(input);
+        Day14.tiltWest(c);
+        var res = Day14.getStringFromCharArray(c);
+
+        assertThat(res)
+                .isEqualTo("""
+                        .#O
+                        O..
+                        OO.
+                        """);
+    }
+
+
+    @Test
+    void testEast() {
+
+        String input = """
+                .#O
+                O..
+                OO.
+                """;
+
+        var c = InputHelper.getCharMatrixFromInput(input);
+        Day14.tiltEast(c);
+        var res = Day14.getStringFromCharArray(c);
+
+        assertThat(res)
+                .isEqualTo("""
+                        .#O
+                        ..O
+                        .OO
+                        """);
+    }
+
+    @Test
+    void testCycle() {
+
+        assertThat(day14.cycle(Input.SMALL, 1)).isEqualTo("""
+                .....#....
+                ....#...O#
+                ...OO##...
+                .OO#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#....
+                ......OOOO
+                #...O###..
+                #..OO#....
                 """);
+
+        assertThat(day14.cycle(Input.SMALL, 2)).isEqualTo("""
+                .....#....
+                ....#...O#
+                .....##...
+                ..O#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#...O
+                .......OOO
+                #..OO###..
+                #.OOO#...O
+                """);
+
+        assertThat(day14.cycle(Input.SMALL, 3)).isEqualTo("""
+                .....#....
+                ....#...O#
+                .....##...
+                ..O#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#...O
+                .......OOO
+                #...O###.O
+                #.OOO#...O
+                """);
+
     }
 
     @Test
@@ -44,6 +211,12 @@ class Day14Test {
     @Test
     void testPart1Small() {
         assertThat(day14.part1(Input.SMALL)).isEqualTo(136);
+    }
+
+    @Test
+    @Disabled("Because")
+    void testPart2Small() {
+        assertThat(day14.part2(Input.SMALL)).isEqualTo(64);
     }
 
     @Test
