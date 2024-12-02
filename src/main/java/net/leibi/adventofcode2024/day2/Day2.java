f01b123790f3b7ec005790891d08f3acfdbee599
Andreas Leibrock <git@leibi.net>
2024-12-02 18:44:26 +0100

    2024 Day 2 part 2

--- src/main/java/net/leibi/adventofcode2024/day2/Day2.java
+++ src/main/java/net/leibi/adventofcode2024/day2/Day2.java
@@ -1,55 +1,72 @@
 package net.leibi.adventofcode2024.day2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Day2 {
 
-  public static final int MIN_SAFE = 1;
-  public static final int MAX_SAFE = 3;
+    public static final int MIN_SAFE = 1;
+    public static final int MAX_SAFE = 3;
 
-  public long part1(String input) {
-    final var list = input.lines().map(s -> s.split("\\s+")).toList();
+    public long part1(String input, boolean dampener) {
+        final var list = input.lines().map(s -> s.split("\\s+")).toList();
 
-    long cnt = 0;
-    for (String[] strings : list) {
-      var integerList = Arrays.stream(strings).map(Integer::parseInt).toList();
-      if (isSafe(integerList)) {
-        cnt++;
-      }
+        long cnt = 0;
+        for (String[] strings : list) {
+            var integerList = Arrays.stream(strings).map(Integer::parseInt).toList();
+            if (isSafe(integerList, dampener)) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
-    return cnt;
-  }
 
-  boolean isSafe(List<Integer> report) {
+    boolean isSafe(List<Integer> report, boolean dampener) {
 
-    Direction direction = getDirection(getDiff(report, 1));
-    boolean returnValue = true;
-    for (var i = 1; i < report.size(); i++) {
-      var diff = getDiff(report, i);
-      if (isNotSafeDistance(diff)) return false;
-
-      Direction localDir = getDirection(diff);
-      if (localDir != direction) return false;
+        Direction direction = getDirection(getDiff(report, 1));
+        final var failedIndices = getFailedIndices(report, direction);
+        if (dampener && !failedIndices.isEmpty()) {
+            for (var i = 0; i < report.size(); i++) {
+                var clone = new ArrayList<>(report);
+                clone.remove(i);
+                if (isSafe(clone, false)) return true;
+            }
+        }
+        return failedIndices.isEmpty();
     }
-    return returnValue;
-  }
 
-  private static int getDiff(List<Integer> report, int i) {
-    return report.get(i) - report.get(i - 1);
-  }
+    private static Set<Integer> getFailedIndices(List<Integer> report, Direction direction) {
+        Set<Integer> failedIndices = new HashSet<>();
+        for (var i = 1; i < report.size(); i++) {
+            var diff = getDiff(report, i);
+            if (isNotSafeDistance(diff)) {
+                failedIndices.add(i - 1);
+            }
 
-  private static Direction getDirection(int diff) {
-    return diff > 0 ? Direction.INC : Direction.DEC;
-  }
+            Direction localDir = getDirection(diff);
+            if (localDir != direction) {
+                failedIndices.add(i - 1);
+            }
+        }
+        return failedIndices;
+    }
 
-  private static boolean isNotSafeDistance(int diff) {
-    return Math.abs(diff) > MAX_SAFE || Math.abs(diff) < MIN_SAFE;
-  }
+    private static int getDiff(List<Integer> report, int i) {
+        return report.get(i) - report.get(i - 1);
+    }
 
-  enum Direction {
-    INC,
-    DEC,
-    BOTH
-  }
+    private static Direction getDirection(int diff) {
+        return diff > 0 ? Direction.INC : Direction.DEC;
+    }
+
+    private static boolean isNotSafeDistance(int diff) {
+        return Math.abs(diff) > MAX_SAFE || Math.abs(diff) < MIN_SAFE;
+    }
+
+    enum Direction {
+        INC, DEC, BOTH
+    }
 }
--- src/test/java/net/leibi/adventofcode2024/day2/Day2Test.java
+++ src/test/java/net/leibi/adventofcode2024/day2/Day2Test.java
@@ -1,53 +1,64 @@
 package net.leibi.adventofcode2024.day2;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class Day2Test {
 
-  private final Day2 day2 = new Day2();
+    private final Day2 day2 = new Day2();
 
-  @Test
-  void part1Small() {
-    assertThat((day2.part1(Input.SMALL))).isEqualTo(2L);
-  }
+    @Test
+    void part1Small() {
+        assertThat((day2.part1(Input.SMALL, false))).isEqualTo(2L);
+    }
 
-  @Test
-  void part1Big() {
-    assertThat((day2.part1(Input.BIG))).isEqualTo(624L);
-  }
+    @Test
+    void part1Big() {
+        assertThat((day2.part1(Input.BIG, false))).isEqualTo(624L);
+    }
 
-  @Test
-  void part2Small() {
-    assertThat((day2.part1(Input.SMALL))).isEqualTo(4L);
-  }
+    @Test
+    void part2Small() {
+        assertThat((day2.part1(Input.SMALL, true))).isEqualTo(4L);
+    }
 
-  @Test
-  void part2Big() {
-    assertThat((day2.part1(Input.BIG))).isEqualTo(624L);
-  }
+    @Test
+    void part2Big() {
+        assertThat((day2.part1(Input.BIG, true))).isEqualTo(658L);
+    }
 
-  @Test
-  void isSafe() {
+    @Test
+    void isSafe() {
 
-    assertThat(day2.isSafe(List.of(7, 6, 4, 2, 1), false)).isTrue();
-    assertThat(day2.isSafe(List.of(1, 2, 7, 8, 9), false)).isFalse();
-    assertThat(day2.isSafe(List.of(9, 7, 6, 2, 1), false)).isFalse();
-    assertThat(day2.isSafe(List.of(1, 3, 2, 4, 5), false)).isFalse();
-    assertThat(day2.isSafe(List.of(8, 6, 4, 4, 1), false)).isFalse();
-    assertThat(day2.isSafe(List.of(1, 3, 6, 7, 9), false)).isTrue();
-  }
+        assertThat(day2.isSafe(List.of(7, 6, 4, 2, 1), false)).isTrue();
+        assertThat(day2.isSafe(List.of(1, 2, 7, 8, 9), false)).isFalse();
+        assertThat(day2.isSafe(List.of(9, 7, 6, 2, 1), false)).isFalse();
+        assertThat(day2.isSafe(List.of(1, 3, 2, 4, 5), false)).isFalse();
+        assertThat(day2.isSafe(List.of(8, 6, 4, 4, 1), false)).isFalse();
+        assertThat(day2.isSafe(List.of(1, 3, 6, 7, 9), false)).isTrue();
+    }
 
-  @Test
-  void isSafeWithDampener() {
+    @Test
+    void isSafeWithDampener() {
 
-    assertThat(day2.isSafe(List.of(7, 6, 4, 2, 1), true)).isTrue();
-    assertThat(day2.isSafe(List.of(1, 2, 7, 8, 9), true)).isFalse();
-    assertThat(day2.isSafe(List.of(9, 7, 6, 2, 1), true)).isFalse();
-    assertThat(day2.isSafe(List.of(1, 3, 2, 4, 5), true)).isTrue();
-    assertThat(day2.isSafe(List.of(8, 6, 4, 4, 1), true)).isTrue();
-    assertThat(day2.isSafe(List.of(1, 3, 6, 7, 9), true)).isTrue();
-  }
+        assertThat(day2.isSafe(List.of(7, 6, 4, 2, 1), true)).isTrue();
+        assertThat(day2.isSafe(List.of(1, 2, 7, 8, 9), true)).isFalse();
+        assertThat(day2.isSafe(List.of(9, 7, 6, 2, 1), true)).isFalse();
+        assertThat(day2.isSafe(List.of(1, 3, 2, 4, 5), true)).isTrue();
+        assertThat(day2.isSafe(List.of(8, 6, 4, 4, 1), true)).isTrue();
+        assertThat(day2.isSafe(List.of(1, 3, 6, 7, 9), true)).isTrue();
+    }
+
+    @Test
+    void isSafeWithDampenerExample() {
+        assertThat(day2.isSafe(List.of(1, 3, 2, 4, 5), true)).isTrue();
+    }
+
+    @Test
+    void isSafeWithDampenerExample2() {
+        assertThat(day2.isSafe(List.of(8, 6, 4, 4, 1), true)).isTrue();
+    }
 }
